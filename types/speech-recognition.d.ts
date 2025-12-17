bdb107e15a629507094003a62d14376756df654b
abhideepghosh <gabhiaf98@gmail.com>
2025-12-17 17:25:30 +0530

    Fixed the language issue.

--- types/speech-recognition.d.ts
+++ types/speech-recognition.d.ts
@@ -6,8 +6,27 @@ declare global {
         webkitSpeechRecognition: any;
     }
 
-    var SpeechRecognition: any;
-    var webkitSpeechRecognition: any;
+    interface SpeechRecognition extends EventTarget {
+        continuous: boolean;
+        interimResults: boolean;
+        lang: string;
+        onstart: ((this: SpeechRecognition, ev: Event) => any) | null;
+        onresult: ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => any) | null;
+        onerror: ((this: SpeechRecognition, ev: SpeechRecognitionErrorEvent) => any) | null;
+        onend: ((this: SpeechRecognition, ev: Event) => any) | null;
+        start(): void;
+        stop(): void;
+        abort(): void;
+    }
+
+    var SpeechRecognition: {
+        prototype: SpeechRecognition;
+        new(): SpeechRecognition;
+    };
+    var webkitSpeechRecognition: {
+        prototype: SpeechRecognition;
+        new(): SpeechRecognition;
+    };
 
     interface SpeechRecognitionErrorEvent extends Event {
         error: string;
